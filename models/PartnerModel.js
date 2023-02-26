a2d37d95aa9e2953f75c26fb57bda1b014eff7ef
Ing-Javier <uniricardo@hotmail.com>
2023-02-25 22:33:32 -0500

    Se crea partner model

---
+++ models/PartnerModel.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose'
+const { Schema, model } = mongoose
+
+export const types = {
+  CLIENTE: 'CLIENTE',
+  DISTRIBUIDOR: 'DISTRIBUIDOR'
+}
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+      },
+      documentType: {
+        type: String,
+        required: true
+      },
+      partnerId: {
+        type: Number,
+        required: true,
+        unique: true
+      },
+    phone: {
+      type: String,
+      required: true
+    },
+    address: {
+      type: String,
+      required: true
+    },
+    email: {
+        type: String,
+        required: true
+      },
+    type: {
+      type: String,
+      enum: Object.keys(types),
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+export const UserModel = model('user', UserSchema, 'user')
